a5abe538c476155634ae469b471e16cf85f08d20
Érica Vieira <ericaj.vieira13@gmail.com>
2023-12-14 16:03:00 -0300

    Inicia criação de usuário

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
-const app = express();
+const routes = require("./routes/index.js")
 
-app.get("/", (request, response) => {
-    response.send("Hello Word!")
-});
+const app = express();
+app.use(express.json());
+app.use(routes);
+
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
