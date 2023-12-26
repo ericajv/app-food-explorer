628f1ac5bd2feaeccf27760cc8d56da92f388afa
Érica Vieira <ericaj.vieira13@gmail.com>
2023-12-26 17:04:20 -0300

    Funcionalidade permite criar usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,16 +1,27 @@
+const { hash } = require("bcryptjs");
+const sqliteConnection = require("../database/sqlite");
 const AppError = require("../utils/AppError");
 
 
 class UsersController {
 
-    create(request, response) {
-        const { name, email, password } = request.body;
+    async create(request, response) {
+        const { name, email, password, role } = request.body;
 
-        if (!name) {
-            throw new AppError("O nome é obrigatório");
+        const database = await sqliteConnection();
+        const checkUsersExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if (checkUsersExists) {
+            throw new AppError("Este email ja está em uso");
         }
 
-        response.status(201).json({ name, email, password });
+        const hashedPassword = await hash(password, 8);
+
+        await database.run("INSERT INTO users (name, email, password, role) VALUES (?,?,?,?)",
+            [name, email, hashedPassword, role]
+        );
+
+        response.status(201).json();
     }
 
 
