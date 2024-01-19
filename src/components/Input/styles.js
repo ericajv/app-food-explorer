3fbbf8077696a14076d852767f63712a1da37c19
Érica Vieira <ericaj.vieira13@gmail.com>
2024-01-19 09:45:45 -0300

    Cria componente Input

---
+++ src/components/Input/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+width: 100%;
+display: flex;
+align-Items: center;
+padding: 0 14px;
+gap: 8px;
+
+background-Color: ${({theme}) => theme.COLORS.DARK_900};
+border-Radius: 5px;
+
+> input {
+    height: 48px;
+    width: 100%;
+    border: none;
+    
+
+    background-Color: ${({theme}) => theme.COLORS.DARK_900};
+    color: ${({theme}) => theme.COLORS.LIGHT_300};
+    
+    
+    &:placeholder{
+        color: ${({theme}) => theme.COLORS.LIGHT_500};
+        
+    }
+
+}
+
+
+`;
