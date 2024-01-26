680c84a5dd76e0ad15ad21682750a83b9a63f7a7
Érica Vieira <ericaj.vieira13@gmail.com>
2024-01-26 12:06:12 -0300

    Inicia tela Prato

--- src/pages/Prato/index.jsx
+++ src/pages/Prato/index.jsx
@@ -1,12 +1,14 @@
 import {Container} from './styles';
+import {Header} from '../../components/Header'
 import {Button} from '../../components/Button'
+import {Footer} from '../../components/Footer'
 
 export function Prato(){
   return(
     <Container>
-    <h1> hello world</h1>
-    <Button title = "Editar prato" loading/>
-    <span>erica</span>
-  </Container>
+     <Header/>
+    <Button title = "Editar prato"/>
+    <Footer/>
+    </Container>
   )
 }
--- src/pages/Prato/styles.js
+++ src/pages/Prato/styles.js
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+
+    display: grid;
+    grid-template-rows: 105px auto 77px;
+    grid-template-areas:
+    "header"
+    "content"
+    "footer"
+
 
 `;
