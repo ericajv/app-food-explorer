9019d979fd189b828c8c3083e598848fb6b7d0e1
Érica Vieira <ericaj.vieira13@gmail.com>
2024-02-16 15:00:51 -0300

    Adiciona Page Prato

--- src/pages/Prato/index.jsx
+++ src/pages/Prato/index.jsx
@@ -1,14 +1,44 @@
-import {Container} from './styles';
-import {Header} from '../../components/Header'
-import {Button} from '../../components/Button'
-import {Footer} from '../../components/Footer'
+import { Container,ButtonBack, Details,TagList, Increment,Amount, Description } from './styles';
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { Footer } from '../../components/Footer'
+import { Tag } from '../../components/Tag'
+import { IoChevronBackOutline } from "react-icons/io5";
+import { GoPlus } from "react-icons/go";
+import { FiMinus } from "react-icons/fi";
 
-export function Prato(){
-  return(
+export function Prato() {
+  return (
     <Container>
-     <Header/>
-    <Button title = "Editar prato"/>
-    <Footer/>
+      <Header />
+      <ButtonBack>
+      <Button icon={IoChevronBackOutline} title="voltar" />
+      </ButtonBack>
+      <Details>
+      <img src="public\assets\Mask group.png" alt="foto prato comida" />
+      <Description>
+        <p className='title'>Salada Ravanello</p>
+        <span>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. </span>
+        <TagList>
+        <Tag title="alface"></Tag>
+        <Tag title="cebola"></Tag>
+        <Tag title="pão naan"></Tag>
+        <Tag title="penino"></Tag>
+        <Tag title="rabanete"></Tag>
+        <Tag title="tomate"></Tag>
+        </TagList>
+            
+        <Increment>
+                    <Amount>
+                        <FiMinus />
+                        <p>01</p>
+                        <GoPlus />
+                    </Amount>
+                    <Button title="Incluir" />
+                </Increment>
+      </Description>
+      </Details>
+      <Footer />
     </Container>
   )
 }
--- src/pages/Prato/styles.js
+++ src/pages/Prato/styles.js
@@ -1,15 +1,112 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.div`
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+      grid-template-rows: 105px 86px auto 77px;
+      height: 100%;
+      }
     width: 100%;
     height: 100vh;
-
     display: grid;
-    grid-template-rows: 105px auto 77px;
+    grid-template-rows: 105px 100px auto 77px;
     grid-template-areas:
     "header"
-    "content"
+    "ButtonBack"
+    "details"
     "footer"
-
-
+    `;
+export const ButtonBack = styled.div`
+>button{
+    width: 106px;
+    font-Size: 1.5rem;
+    font-Weight: bold;
+    display: flex;
+    align-Items: center;
+    text-Align: center;
+    margin-Top: 32px;
+    margin-Left: 122px;
+    padding: 5px;
+    background: none;
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+      margin-Top: 16px;
+      margin-Left: 56px;
+    }
+}
 `;
+export const Details = styled.div`
+ display: flex;
+ gap:47px;
+ margin-Left: 7.6rem;
+ margin-Right: 7.6rem;
+background-Color: none;
+> img{
+    width:24.375rem;
+   height: 24.3125rem; 
+   @media (max-width: ${DEVICE_BREAKPOINTS.MD}) { 
+    width:10rem;
+   height: 10rem; 
+   }
+}
+@media (max-width: ${DEVICE_BREAKPOINTS.MD}) {      
+display:flex;
+align-Items:center;
+flex-Direction:column;
+gap:16px;
+width: 316px;
+margin: auto;
+}
+`;
+export const Description = styled.div`
+display: flex;
+flex-Direction: column;
+margin-Top: 45px;
+gap:24px;
+@media (max-width: ${DEVICE_BREAKPOINTS.MD}) {  
+  margin-Top: 0px;
+  text-Align: center;
+ }
+> p{
+    font-Size: 2.5rem;
+    white-space: nowrap;
+   }
+>span{
+    font-Size: 1.5rem;
+    font-Weight: 300;
+}
+`;
+export const TagList = styled.div`
+display: flex;
+  flex-direction: row;
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+    flex-wrap: wrap;
+    gap:24px;
+  }
+`;
+export const Increment = styled.div`
+    background-Color: transparent;
+      width: 294px;
+      display: flex;
+      align-Items: center;
+      gap:2.06rem;
+      font-Size: 20px;
+      font-Weight: 400;
+      margin-Top: 24px;
+        @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {      
+      width: 316px;
+      margin-Bottom: 55px;
+      }
+    `;
+export const Amount = styled.div`
+    background-Color: transparent;
+    display: flex;
+    align-Items: center;
+    gap:14px;
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+    height: 32px;
+        > svg{
+        width: 1.5rem;
+        height: 1.5rem;
+      }
+    }
+    `;
