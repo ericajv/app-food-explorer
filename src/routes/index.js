76d1dc2a87009d0b6f93ba94d77df2f5b8c218b6
Érica Vieira <ericaj.vieira13@gmail.com>
2024-02-17 19:32:06 -0300

    Inicia autenticação

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
 const platesRoutes = require("./plates.routes");
+const sessionsRoutes = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/plates", platesRoutes);
+routes.use("/sessions", sessionsRoutes);
 
 module.exports = routes;
