22cd8b892707c625b040f39cddd1a63d9a552a2d
ericajv <ericaj.vieira13@gmail.com>
2024-02-21 14:41:39 -0300

    add axios and backend communication

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,17 +1,35 @@
 import { Container, Logo, Form } from "./styles";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 import { ButtonText } from "../../components/ButtonText"
+import { useState } from "react";
+import { api } from "../../services/api";
 
 export function Signup() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     const navigate = useNavigate()
 
     function navigateToLogin() {
         navigate("/")
     }
-  
-    
+
+    async function handleSignUp() {
+        if (!name || !email || !password) {
+            return alert("Preencha todos os campos");
+        }
+
+        api.post("/users", { name, email, password })
+            .then(() => {
+                alert("Usuário cadastrado com sucesso");
+                navigate("/");
+            })
+            .catch(error => alert(error.response.data.message || "Não foi possível cadastrar"));
+    }
+
     return (
         <Container>
             <Logo>
@@ -24,19 +42,22 @@ export function Signup() {
                 <Input
                     placeholder="Exemplo: Maria da Silva"
                     type="text"
-                ></Input>
+                    onChange={e => setName(e.target.value)}
+                />
                 <label htmlFor="email">Email</label>
                 <Input
                     placeholder="Exemplo: exemplo@exemplo.com.br"
                     type="email"
-                ></Input>
+                    onChange={e => setEmail(e.target.value)}
+                />
                 <label htmlFor="password">Senha</label>
                 <Input
                     placeholder="No mínimo 6 caracteres"
                     type="password"
-                ></Input>
-                <Button title="Criar conta"></Button>
-                <ButtonText onClick={navigateToLogin} title="Já tenho uma conta"></ButtonText>
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <Button onClick={handleSignUp} title="Criar conta"/>
+                <ButtonText onClick={navigateToLogin} title="Já tenho uma conta"/>
             </Form>
         </Container>
     );
